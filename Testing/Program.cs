fd57dcab14a6a3edb7023eee58ef3d159febc595
Mike Baugus <mbaugus@gmail.com>
2018-02-19 15:38:57 -0500

    huh

--- Testing/Program.cs
+++ Testing/Program.cs
@@ -4,9 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace RPNCalculator
+namespace Testing
 {
-    public class Class1
+    class Program
     {
+        static void Main(string[] args)
+        {
+
+        }
     }
 }
